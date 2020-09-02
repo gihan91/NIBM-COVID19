cbec33a80c84809a6406ee5e57395b7435937050
Gihan <gihan@ceylonsolutions.com>
2020-09-02 12:13:23 +0530

    Initial commit

---
+++ NIBM-COVID19/NIBM-COVID19/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NIBM-COVID19
+//
+//  Created by Gihan on 9/2/20.
+//  Copyright © 2020 Gihan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
